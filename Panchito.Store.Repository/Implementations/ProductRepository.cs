8131e20c26158d99bd3f845e64c360d4b2bc4b3c
Richard Negron <richard.negron.257@gmail.com>
2023-03-20 02:27:43 -0500

    repositorio con dapper y oracle

--- Panchito.Store.Repository/Implementations/ProductRepository.cs
+++ Panchito.Store.Repository/Implementations/ProductRepository.cs
@@ -1,18 +1,38 @@
-﻿using Panchito.Store.Entity;
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using Oracle.ManagedDataAccess.Client;
+using Panchito.Store.Entity;
 using Panchito.Store.Repository.Interfaces;
 
 namespace Panchito.Store.Repository.Implementations
 {
     public class ProductRepository : IProductRepository
     {
-        public Task<Product> GetProductByIdAsync(int id)
+        private readonly string _connectionString;
+
+        public ProductRepository(IConfiguration configuration)
         {
-            throw new NotImplementedException();
+            _connectionString = configuration.GetConnectionString("OracleConnection");
         }
 
-        public Task<List<Product>> GetProductsAsync()
+        public async Task<List<Product>> GetProductsAsync()
         {
-            throw new NotImplementedException();
+            using (var connection = new OracleConnection(_connectionString))
+            {
+                var sql = "SELECT * FROM Product";
+                var products = await connection.QueryAsync<Product>(sql);
+                return products.ToList();
+            }
+        }
+
+        public async Task<Product> GetProductByIdAsync(int id)
+        {
+            using (var connection = new OracleConnection(_connectionString))
+            {
+                var sql = $"SELECT * FROM product WHERE id = {id}";
+                var product = await connection.QueryFirstAsync<Product>(sql);
+                return product;
+            }
         }
     }
 }
