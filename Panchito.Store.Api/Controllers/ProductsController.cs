5107600fd2871ac9c70064ec11650e3d694a54dc
Richard Negron <richard.negron.257@gmail.com>
2023-03-20 03:33:27 -0500

    api creada

---
+++ Panchito.Store.Api/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Panchito.Store.Application.Dtos;
+using Panchito.Store.Application.Interfaces;
+
+namespace Panchito.Store.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductApplication _productApplication;
+
+        public ProductsController(IProductApplication productApplication)
+        {
+            _productApplication = productApplication;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<ProductDto>>> GetProducts()
+        {
+            var products = await _productApplication.GetProductsAsync();
+            return products;
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<ProductDto>> GetProductById(int id)
+        {
+            var product = await _productApplication.GetProductByIdAsync(id);
+            return product;
+        }
+    }
+}
--- Panchito.Store.Api/Program.cs
+++ Panchito.Store.Api/Program.cs
@@ -1,6 +1,15 @@
+using Panchito.Store.Application.Implementations;
+using Panchito.Store.Application.Interfaces;
+using Panchito.Store.Application.Profiles;
+using Panchito.Store.Repository.Implementations;
+using Panchito.Store.Repository.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddAutoMapper(typeof(MappingProfile));
+builder.Services.AddScoped<IProductApplication, ProductApplication>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
